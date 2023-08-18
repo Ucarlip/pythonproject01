28b387f9c88b7047e03863ae6049904242fe5a92
Ucarlip <modkak98@gmail.com>
2023-08-18 11:16:02 +0700

    code V.1 :creat 3 file

---
+++ test03.py
@@ -0,0 +1,12 @@
+print('AAAA') ; print("BBBB")
+print("CCC")
+print("DDD\nEEE\nFFF")
+print("DDD\tEEE\tFFF")
+print('Welcome "SAu"')
+print("Welcome 'iot'")
+
+print('''
+      sau
+      iot
+      wow
+      ''')
